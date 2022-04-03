41727e57ceea2771f25b549674c09714c0013f17
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-04-03 13:57:59 +0400

    Fix warnings because of nullability

--- BusinessModel/Comparers/PriceComparer.cs
+++ BusinessModel/Comparers/PriceComparer.cs
@@ -8,9 +8,14 @@ public class PriceComparer : IComparer<AbstractWebItem>
 {
     // Custom comparator function used in sorting of Scrapped Items.
     // Compares 2 AbstractWebItem classes in asceending order.
-    public int Compare(AbstractWebItem x,
-                       AbstractWebItem y)
-    {   
+    public int Compare(AbstractWebItem? x,
+                       AbstractWebItem? y)
+    {   if (x == null || (x.Price == null) && (y == null || y.Price == null))
+            return 0;
+        if (x == null || x.Price == null)
+            return 1;
+        if (y == null || y.Price == null)
+            return -1;
         // return 0 if prices are equal
         if (x.Price.Value == y.Price.Value)
             return 0;
